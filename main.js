58ccba8aaa2caced02cbaaf1047937e7eb5a9652
Lucas Charles <lucascharlesz@hotmail.com>
2017-11-09 16:01:06 -0200

    VueJS aula 023 - Toggle Modal.

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ Vue.component("tarefa", {
 
 Vue.component("message", {
 	props: [
-		"messageTitle", 
+		"message_title", 
 	],
 	template: `
 		<article class="message" v-show="visible">
 		  <div class="message-header">
-		    <p>{{ messageTitle }}</p>
+		    <p>{{ message_title }}</p>
 		    <button class="delete" aria-label="delete" @click="hideMessage"></button>
 		  </div>
 		  <div class="message-body">
@@ -44,6 +44,39 @@ Vue.component("message", {
 	}
 });
 
+
+Vue.component("modal", {
+	template: `
+		<div :class="modal_class">
+		  <div class="modal-background"></div>
+		  <div class="modal-content">
+		  	<div class="box">
+		    	<slot></slot>
+	    	</div>
+		  </div>
+		  <button class="modal-close is-large" aria-label="close" @click="hideModal"></button>
+		</div>
+	`,
+	data() {
+		return {
+			modal_class: "modal",
+		}
+	},
+
+	methods: {
+		hideModal() {
+			this.modal_class = "modal";
+		}
+	}
+});
+
+
 new Vue({
-	el: "#app"
+	el: "#app",
+	methods: {
+		toggleModal() {
+			console.log(this.$refs);
+			this.$refs.alert_modal.modal_class = "modal is-active"
+		}
+	}
 });
