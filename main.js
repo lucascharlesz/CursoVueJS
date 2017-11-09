e2b2460fc78165b8a2f1eb8123ed86a31a880a93
Lucas Charles <lucascharlesz@hotmail.com>
2017-11-09 14:43:51 -0200

    VueJS Aula 022 - Componente de mensagem.

--- main.js
+++ main.js
@@ -16,6 +16,34 @@ Vue.component("tarefa", {
 	template: "<li> <slot></slot> </li>"
 });
 
+Vue.component("message", {
+	props: [
+		"messageTitle", 
+	],
+	template: `
+		<article class="message" v-show="visible">
+		  <div class="message-header">
+		    <p>{{ messageTitle }}</p>
+		    <button class="delete" aria-label="delete" @click="hideMessage"></button>
+		  </div>
+		  <div class="message-body">
+		    <slot></slot>
+		  </div>
+		</article>
+	`,
+	data() {
+		return {
+			visible: true,
+		}
+	},
+
+	methods: {
+		hideMessage() {
+			this.visible = false;
+		}
+	}
+});
+
 new Vue({
 	el: "#app"
 });
