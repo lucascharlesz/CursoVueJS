9f1c76a466dcb58b06a51093ad7f9e39f372a82e
Lucas Charles <lucascharlesz@hotmail.com>
2017-11-10 12:17:02 -0200

    Aula 030.

---
+++ src/components/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Address: {{ address }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'contact',
+    data () {
+      return {
+        address: 'Rua Álvaro Ramos, 86'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
